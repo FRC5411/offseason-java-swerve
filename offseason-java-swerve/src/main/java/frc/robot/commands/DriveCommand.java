2fdf95e1debf3ba3f903c77282b9a773868167c3
RDc12 <52055654+RDc12Programming@users.noreply.github.com>
2022-09-10 22:33:34 -0500

    Remove unnecessary command end case

--- offseason-java-swerve/src/main/java/frc/robot/commands/DriveCommand.java
+++ offseason-java-swerve/src/main/java/frc/robot/commands/DriveCommand.java
@@ -59,9 +59,7 @@ public class DriveCommand extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    m_swerve.drive(0, 0, 0);
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
