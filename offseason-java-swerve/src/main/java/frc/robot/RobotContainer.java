d459f274d0a5b31b943eaf17fb90edf8bc0cfa99
RDc12 <52055654+RDc12Programming@users.noreply.github.com>
2022-10-27 20:04:09 -0500

    Add Test Path for pathplanner

--- offseason-java-swerve/src/main/java/frc/robot/RobotContainer.java
+++ offseason-java-swerve/src/main/java/frc/robot/RobotContainer.java
@@ -31,7 +31,7 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final Swerve m_swerve = new Swerve(pigeon);
 
-  private final Command m_autoCommand = m_swerve.followTrajectoryCommand(PathPlanner.loadPath("New Path", new PathConstraints(4, 3)));
+  private final Command m_autoCommand = m_swerve.followTrajectoryCommand(PathPlanner.loadPath("Test Path", new PathConstraints(4, 3)));
 
   JoystickButton aButton = new JoystickButton(driver, 1);
   JoystickButton bButton = new JoystickButton(driver, 2);
